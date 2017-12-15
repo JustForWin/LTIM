a15ef7b3d61d7db001af4d2b91820bc95242a730
yanghenguser <1565845039@qq.com>
2017-12-15 11:37:39 +0800

    finish

--- js/login.js
+++ js/login.js
@@ -1,3 +1,7 @@
 $(function(){
 	//UIkit.icon('.toprightico').svg.then(function(svg) { svg.querySelector('path').style.stroke = 'white'; })
+	
+	$('#loginbtn').click(function(){
+		window.location.href = "http://localhost:8080/LTIMServer?name="+$('#num').val()+"&psw="+$("#pwd").val();
+	})
 })
